faa00a4ad88d0598f732ca7c390f145bade24dc5
CragglesG <121510295+CragglesG@users.noreply.github.com>
2024-12-23 20:50:45 +0000

    Add temperature to weather tuple and sort by position

--- example-project/README.md
+++ example-project/README.md
@@ -3,4 +3,4 @@
 > [!WARNING]
 > This project is an experimental WIP and will not always function. It is intended only as an example.
 
-This project is an example of a Hack RTL submission. It is a simple project that demonstrates the use of an RTL-SDR dongle to estimate weather conditions in the area surrounding the user with ADS-B.
+This project is an example of a Hack RTL submission. It is a simple project that demonstrates the use of an RTL-SDR dongle to estimate weather conditions in an area using ADS-B.
--- example-project/main.py
+++ example-project/main.py
@@ -1,5 +1,6 @@
 import os
-import pyModeS
+from numpy import float64
+import pyModeS as pms
 from dotenv import load_dotenv
 from rtlsdr import RtlSdrTcpClient
 
@@ -7,6 +8,13 @@ load_dotenv()
 
 LATITUDE = int(os.getenv("SERVER_LATITUDE", 0))
 LONGITUDE = int(os.getenv("SERVER_LONGITUDE", 0))
+
+directions = {"N": 0, "NNE": 22.5, "NE": 45, "ENE": 67.5, "E": 90, "ESE": 112.5, "SE": 135, "SSE": 157.5, "S": 180, "SSW": 202.5, "SW": 225, "WSW": 247.5, "W": 270, "WNW": 292.5, "NW": 315, "NNW": 337.5}
+
+def get_direction(degrees):
+    for direction, degree in directions.items():
+        if degree - 11.25 < degrees < degree + 11.25:
+            return direction
 
 sdr = RtlSdrTcpClient(hostname=os.getenv("SERVER_HOST", "0.0.0.0"), port=int(os.getenv("SERVER_PORT", 8000)))
 
@@ -21,5 +29,13 @@ weather = []
 
 for msg in samples:
     msg = str(msg)
-    pyModeS.bds.infer(msg, mrar=True)
-    weather.append((pyModeS.adsb.surface_position_with_ref(msg, LATITUDE, LONGITUDE), pyModeS.adsb.altitude(msg), pyModeS.commb.hum44(msg)))
+    if pms.df(msg) != 17:
+        continue
+    pms.bds.infer(msg, mrar=True)
+    alt = pms.adsb.altitude(msg) if type(pms.adsb.altitude(msg)) == float else 30000.0
+    temp = pms.commb.temp44(msg)[0] + (1.98 * (alt / 1000))
+    weather.append((pms.adsb.surface_position_with_ref(msg, LATITUDE, LONGITUDE), alt, pms.commb.hum44(msg), pms.commb.p44(msg), get_direction(pms.commb.wind44(msg)[1]), temp))
+
+weather.sort(key=lambda x: x[0])
+
+# TODO: display weather on an interactive map on a locally served webpage
